17d78e16ee4af9f19b62857242bc2e4cd6e32043
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-11-28 18:54:04 +0100

    LEKCJA 6: Tworzenie Widoków

---
+++ MyTasks/Core/ViewModels/TaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MyTasks.Core.Models.Domains;
+using System.Collections.Generic;
+
+namespace MyTasks.Core.ViewModels
+{
+	public class TaskViewModel
+	{
+		public string Heading { get; set; }
+		public TaskEntity Task { get; set; }
+		public IEnumerable<Category> Categories { get; set; }
+	}
+}
